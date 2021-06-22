a427403e20ccf5a43e2e0cbbc3bcea3eeecd7e1f
Vivian Li <Vivian.Li@softwire.com>
2021-06-22 09:43:43 +0100

    Prints FizzBuzz numbers from 1 to 100

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -1,5 +1,22 @@
 package com.example.fizzbuzz
 
+fun fizzBuzz(n: Int): String {
+    var result: String = ""
+    if (!(n % 3 == 0) && !(n % 5 == 0)) {
+        result += "$n"
+    } else {
+        if (n % 3 == 0) {
+            result += "Fizz"
+        }
+        if (n % 5 == 0) {
+            result += "Buzz"
+        }
+    }
+    return result
+}
+
 fun main() {
-    print("Hello world!")
+    for (n in 1..100) {
+        print(fizzBuzz(n))
+    }
 }
